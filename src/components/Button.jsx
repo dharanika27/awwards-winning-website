193c66a4aebb58b8fecca561125314560bedac20
Dharanika <dharanika27112003@gmail.com>
2025-07-26 12:23:13 +0530

    first commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
     return (
-        <buttonn id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+        <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
             {leftIcon}
 
             <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
@@ -11,7 +10,7 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
                 </div>
             </span>
             {rightIcon}
-        </buttonn>
+        </button>
     )
 }
 export default Button
